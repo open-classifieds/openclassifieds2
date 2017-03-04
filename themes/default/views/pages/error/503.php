512c29cfcc92f4de47afa90ba9791159af9cce4f
cybernet <cybernet2u@yahoo.com>
2017-03-04 08:48:55 +0200

    boot 3.3.7

--- themes/default/views/pages/error/503.php
+++ themes/default/views/pages/error/503.php
@@ -6,12 +6,12 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="We are working on our site, please visit later. Thanks">
+    <meta name="author" content="Yclas">
 
     <title><?=Core::config('general.site_name')?> - <?=__('Maintenance')?></title>
 
-    <link href="//cdn.jsdelivr.net/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
   </head>
 
