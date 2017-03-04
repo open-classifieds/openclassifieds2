1b68abf998092d762bdb3e9fd5e28411596b5d6a
cybernet <cybernet2u@yahoo.com>
2017-03-04 08:41:13 +0200

    bootstrap 3.3.7

--- themes/default/views/pages/error/503.php
+++ themes/default/views/pages/error/503.php
@@ -11,7 +11,7 @@
 
     <title><?=Core::config('general.site_name')?> - <?=__('Maintenance')?></title>
 
-    <link href="//cdn.jsdelivr.net/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
   </head>
 
