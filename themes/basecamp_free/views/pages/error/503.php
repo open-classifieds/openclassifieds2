4b7ef9b0059f548f62537f737bee2fac4cd53cdd
cybernet <cybernet2u@yahoo.com>
2017-03-04 09:50:02 +0200

    bootstrap 3.3.7

--- themes/basecamp_free/views/pages/error/503.php
+++ themes/basecamp_free/views/pages/error/503.php
@@ -11,7 +11,7 @@
     <meta name="robots" content="noindex">
     <meta name="googlebot" content="noindex">
     <title><?=Core::config('general.site_name')?> - <?=__('Maintenance')?></title>
-    <link href="//cdn.jsdelivr.net/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 </head>
 <body>
     <style type="text/css">
