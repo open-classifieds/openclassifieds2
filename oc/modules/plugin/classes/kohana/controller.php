7bb86310fa1d52abbbf697db67e8597afe35e95c
Blackout <blackout@drunkenlords.com>
2019-04-05 13:56:14 +1000

    Update controller.php
    Updated controller to latest Kohana_Controller code - would fail previously when module was enabled

--- oc/modules/plugin/classes/kohana/controller.php
+++ oc/modules/plugin/classes/kohana/controller.php
@@ -14,7 +14,12 @@
  * @copyright  (c) 2009-2013 Open Classifieds Team
  * @license    GPL v3
  */
-abstract class Kohana_Controller  {
+
+// Blackout - 11:47 AM 5/04/2019
+// * Updated to reflect new changes to the original Kohana_Controller i.e. execute() now exists
+// * Changed Hook::do_action() calls to now pass $this instead of $this->request->param() - now we can actually modify the controllers content from a hook!
+
+abstract class Kohana_Controller {
 
 	/**
 	 * @var  Request  Request that created the controller
@@ -30,7 +35,7 @@ abstract class Kohana_Controller  {
 	 * @var Hook name
 	 */
 	protected $hook;
-	
+
 	/**
 	 * Creates a new controller instance. Each controller must be constructed
 	 * with the request object that created it.
@@ -47,29 +52,102 @@ abstract class Kohana_Controller  {
 		// Assign a response to the controller
 		$this->response = $response;
 		
+		// Hook
 		$this->hook = $this->request->controller().'_'.$this->request->action();
-		///var_dump($this->hook);
-		Hook::do_action($this->hook,$this->request->param());
+		Hook::do_action($this->hook, [$this]);
 	}
 
 	/**
-	 * Automatically executed before the controller action.
-	 * 
+	 * Executes the given action and calls the [Controller::before] and [Controller::after] methods.
+	 *
+	 * Can also be used to catch exceptions from actions in a single place.
+	 *
+	 * 1. Before the controller action is called, the [Controller::before] method
+	 * will be called.
+	 * 2. Next the controller action will be called.
+	 * 3. After the controller action is called, the [Controller::after] method
+	 * will be called.
+	 *
+	 * @throws  HTTP_Exception_404
+	 * @return  Response
+	 */
+	public function execute()
+	{
+		// Execute the "before action" method
+		$this->before();
+
+		// Determine the action to use
+		$action = 'action_'.$this->request->action();
+
+		// If the action doesn't exist, it's a 404
+		if ( ! method_exists($this, $action))
+		{
+			throw HTTP_Exception::factory(404,
+				'The requested URL :uri was not found on this server.',
+				[':uri' => $this->request->uri()]
+			)->request($this->request);
+		}
+
+		// Execute the action itself
+		$this->{$action}();
+
+		// Execute the "after action" method
+		$this->after();
+
+		// Return the response
+		return $this->response;
+	}
+
+	/**
+	 * Automatically executed before the controller action. Can be used to set
+	 * class properties, do authorization checks, and execute other custom code.
+	 *
 	 * @return  void
 	 */
 	public function before()
 	{
-		Hook::do_action($this->hook.'_before',$this->request->param());
+		Hook::do_action($this->hook.'_before', [$this]);
 	}
 
 	/**
-	 * Automatically executed after the controller action. 
+	 * Automatically executed after the controller action. Can be used to apply
+	 * transformation to the response, add extra output, and execute
+	 * other custom code.
 	 *
 	 * @return  void
 	 */
 	public function after()
 	{
-		Hook::do_action($this->hook.'_after',$this->request->param());
+		Hook::do_action($this->hook.'_after', [$this]);
 	}
 
-} // End Controller
+	/**
+	 * Issues a HTTP redirect.
+	 *
+	 * Proxies to the [HTTP::redirect] method.
+	 *
+	 * @param  string  $uri   URI to redirect to
+	 * @param  int     $code  HTTP Status code to use for the redirect
+	 * @throws HTTP_Exception
+	 */
+	public static function redirect($uri = '', $code = 302)
+	{
+		return HTTP::redirect( (string) $uri, $code);
+	}
+
+	/**
+	 * Checks the browser cache to see the response needs to be returned,
+	 * execution will halt and a 304 Not Modified will be sent if the
+	 * browser cache is up to date.
+	 *
+	 *     $this->check_cache(sha1($content));
+	 *
+	 * @param  string  $etag  Resource Etag
+	 * @return Response
+	 */
+	protected function check_cache($etag = NULL)
+	{
+		return HTTP::check_cache($this->request, $this->response, $etag);
+	}
+
+}
