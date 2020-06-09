7a85a0e7ad1f3a30658f624efaa264553bf0ca51
Oliver <oliver@open-classifieds.com>
2020-06-09 18:40:58 -0500

    Move form errors template and alerts to view components

---
+++ themes/default/views/oc-panel/components/error.php
@@ -0,0 +1,1 @@
+<div class="alert "><a class="close" data-dismiss="alert">×</a>%s</div>
---
+++ themes/default/views/oc-panel/components/errors.php
@@ -0,0 +1,3 @@
+<div class="alert alert-danger"><a class="close" data-dismiss="alert">×</a>
+                        <p><strong>%s</strong></p>
+                        <ul>%s</ul></div>
