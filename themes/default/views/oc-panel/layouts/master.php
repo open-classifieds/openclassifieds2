82c0795d154f9d71513c698a9b7fd5a2514d3e8e
Oliver <oliver@open-classifieds.com>
2020-06-11 14:04:00 -0500

    Upgrade bootgrid, related to yclas#3001

--- themes/default/views/oc-panel/layouts/master.php
+++ themes/default/views/oc-panel/layouts/master.php
@@ -32,7 +32,7 @@
 
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script>
     <script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>
-    <script src="/themes/default/js/jquery.bootgrid.min.js"></script>
+    <script src="https://unpkg.com/jquery-bootgrid@1.3.1/dist/jquery.bootgrid.min.js"></script>
     <script src="/themes/default/js/bootstrap-datepicker.js"></script>
 
     <!-- include summernote css/js -->
