3b52b18beb937b4b8029c8ba44e6780ee534cdd1
Oliver <oliver@open-classifieds.com>
2020-06-12 12:03:38 -0500

    Fixes missing social login config, related to yclas#3001

--- themes/default/views/oc-panel/pages/addons/social-login/index.php
+++ themes/default/views/oc-panel/pages/addons/social-login/index.php
@@ -26,6 +26,138 @@
                     <?=FORM::checkbox('is_active', 1, (bool) Core::post('is_active', $is_active), ['class' => 'form-checkbox h-6 w-6 text-blue-600 bg-gray-100 transition duration-150 ease-in-out'])?>
                 </div>
             </div>
+            <div class="mt-8 border-t border-gray-200 pt-8">
+                <div>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                        Facebook
+                    </h3>
+                </div>
+                <div class="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
+                    <div class="sm:col-span-6">
+                        <div class="absolute flex items-center h-5">
+                            <?=FORM::checkbox('facebook_enabled', 1, (bool) Core::post('facebook_enabled', $config['providers']['Facebook']['enabled']), ['class' => 'form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out'])?>
+                        </div>
+                        <div class="pl-7 text-sm leading-5">
+                            <?=FORM::label('facebook_enabled', __('Enabled'), ['class'=>'font-medium text-gray-700'])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('facebook_id', __('Id'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('facebook_id', Core::post('facebook_id', $config['providers']['Facebook']['keys']['id']), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'facebook_id',
+                            ])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('facebook_secret', __('Secret'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('facebook_secret', Core::post('facebook_secret', $config['providers']['Facebook']['keys']['secret']), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'facebook_secret',
+                            ])?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8 border-t border-gray-200 pt-8">
+                <div>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                        Google
+                    </h3>
+                </div>
+                <div class="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
+                    <div class="sm:col-span-6">
+                        <div class="absolute flex items-center h-5">
+                            <?=FORM::checkbox('google_enabled', 1, (bool) Core::post('google_enabled', $config['providers']['Google']['enabled']), ['class' => 'form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out'])?>
+                        </div>
+                        <div class="pl-7 text-sm leading-5">
+                            <?=FORM::label('google_enabled', __('Enabled'), ['class'=>'font-medium text-gray-700'])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('google_id', __('Id'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('google_id', Core::post('google_id', $config['providers']['Google']['keys']['id']), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'google_id',
+                            ])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('google_secret', __('Secret'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('google_secret', Core::post('google_secret', $config['providers']['Google']['keys']['secret']), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'google_secret',
+                            ])?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-8 border-t border-gray-200 pt-8">
+                <div>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">
+                        OAuth2
+                    </h3>
+                </div>
+                <div class="mt-6 grid grid-cols-1 row-gap-6 col-gap-4 sm:grid-cols-6">
+                    <div class="sm:col-span-6">
+                        <div class="absolute flex items-center h-5">
+                            <?=FORM::checkbox('oauth2_enabled', 1, (bool) Core::post('oauth2_enabled', Core::config('social.oauth2_enabled')), ['class' => 'form-checkbox h-4 w-4 text-blue-600 transition duration-150 ease-in-out'])?>
+                        </div>
+                        <div class="pl-7 text-sm leading-5">
+                            <?=FORM::label('oauth2_enabled', __('Enabled'), ['class'=>'font-medium text-gray-700'])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('oauth2_client_id', __('Client id'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('oauth2_client_id', Core::post('oauth2_client_id', Core::config('social.oauth2_client_id')), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'oauth2_client_id',
+                            ])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('oauth2_client_secret', __('Client secret'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('oauth2_client_secret', Core::post('oauth2_client_secret', Core::config('social.oauth2_client_secret')), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'oauth2_client_secret',
+                            ])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('oauth2_url_authorize', __('URL authorize'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('oauth2_url_authorize', Core::post('oauth2_url_authorize', Core::config('social.oauth2_url_authorize')), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'oauth2_url_authorize',
+                            ])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('oauth2_url_access_token', __('URL access token'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('oauth2_url_access_token', Core::post('oauth2_url_access_token', Core::config('social.oauth2_url_access_token')), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'oauth2_url_access_token',
+                            ])?>
+                        </div>
+                    </div>
+                    <div class="sm:col-span-4">
+                        <?= FORM::label('oauth2_url_resource_owner_details', __('URL resource owner details'), array('class'=>'block text-sm font-medium leading-5 text-gray-700'))?>
+                        <div class="mt-1 rounded-md shadow-sm">
+                            <?= FORM::input('oauth2_url_resource_owner_details', Core::post('oauth2_url_resource_owner_details', Core::config('social.oauth2_url_resource_owner_details')), [
+                                'class' => 'form-input block w-full transition duration-150 ease-in-out sm:text-sm sm:leading-5',
+                                'id' => 'oauth2_url_resource_owner_details',
+                            ])?>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="mt-5 sm:flex sm:items-center">
                 <span class="mt-3 w-ful inline-flex rounded-md shadow-sm sm:mt-0 sm:w-auto">
                     <?= Form::button('submit', __('Save'), ['type'=>'submit', 'class'=>'w-full inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150 sm:w-auto sm:text-sm sm:leading-5'])?>
