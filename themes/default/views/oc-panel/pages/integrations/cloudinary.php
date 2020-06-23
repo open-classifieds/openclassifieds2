0fb9803b17be4db21ceed68abc948d3e6547bcfa
Oliver <oliver@open-classifieds.com>
2020-06-23 14:25:21 -0500

    Fix cloudinary typo, related to #3001

--- themes/default/views/oc-panel/pages/integrations/cloudinary.php
+++ themes/default/views/oc-panel/pages/integrations/cloudinary.php
@@ -1,7 +1,7 @@
 <div class="md:flex md:items-center md:justify-between">
     <div class="flex-1 min-w-0">
         <h2 class="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:leading-9 sm:truncate">
-            <?= __('Google Picker') ?>
+            <?= __('Cloudinary') ?>
         </h2>
     </div>
 </div>
