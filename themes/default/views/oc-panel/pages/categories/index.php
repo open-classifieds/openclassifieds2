fc1c855e5524ed2aa343bd92083567bbc53c17f8
Oliver Jiménez Servín <oliverds@users.noreply.github.com>
2020-07-22 11:47:13 -0500

    Delete all categories/locations fix

--- themes/default/views/oc-panel/pages/categories/index.php
+++ themes/default/views/oc-panel/pages/categories/index.php
@@ -148,20 +148,23 @@
     </div>
 <?=Form::close()?>
 
-<div class="bg-white shadow sm:rounded-lg mt-8">
-    <div class="px-4 py-5 sm:p-6">
-        <h3 class="text-lg leading-6 font-medium text-gray-900">
-            <?= __('Delete all categories') ?>
-        </h3>
-        <div class="mt-2 max-w-xl text-sm leading-5 text-gray-500">
-            <p>
-                <?= __('This is permanent! No backups, no restores, no magic undo button. We warned you, ok?') ?>
-            </p>
-        </div>
-        <div class="mt-5">
-            <a href="<?=Route::url('oc-panel', ['controller' => 'category', 'action'=>'delete_all'])?>" role="button" class="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-50 focus:outline-none focus:border-red-300 focus:shadow-outline-red active:bg-red-200 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-                <?= __('Delete') ?>
-            </a>
+<?=Form::open(Route::url('oc-panel', ['controller' => 'category', 'action'=>'delete_all']))?>
+    <div class="bg-white shadow sm:rounded-lg mt-8">
+        <div class="px-4 py-5 sm:p-6">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+                <?= __('Delete all categories') ?>
+            </h3>
+            <div class="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+                <p>
+                    <?= __('This is permanent! No backups, no restores, no magic undo button. We warned you, ok?') ?>
+                </p>
+            </div>
+            <div class="mt-5">
+                <?= Form::hidden('confirmation', 1) ?>
+                <button type="submit" class="inline-flex items-center justify-center px-4 py-2 border border-transparent font-medium rounded-md text-red-700 bg-red-100 hover:bg-red-50 focus:outline-none focus:border-red-300 focus:shadow-outline-red active:bg-red-200 transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+                    <?= __('Delete') ?>
+                </button>
+            </div>
         </div>
     </div>
-</div>
+<?=Form::close()?>
