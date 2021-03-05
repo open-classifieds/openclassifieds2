8562b8fadcb38d58133af5b7f86ec2fc2655aaec
Oliver Jimenez-Servin <oliverds@users.noreply.github.com>
2021-03-05 10:54:08 -0600

    Fix translation string

--- oc/classes/auth/frontcontroller.php
+++ oc/classes/auth/frontcontroller.php
@@ -20,23 +20,23 @@ class Auth_Frontcontroller extends Controller
     {
         //the user was loged in and with the right permissions
         parent::__construct($request,$response);
-    
+
         // Assign the request to the controller
         $this->request = $request;
-    
+
         // Assign a response to the controller
         $this->response = $response;
-    
+
         //login control, don't do it for auth controller so we dont loop
         if ($this->request->controller()!='auth')
         {
             $url_bread = Route::url('oc-panel',array('controller'  => 'profile', 'action'  => 'public'));
             Breadcrumbs::add(Breadcrumb::factory()->set_title(__('My Profile'))->set_url($url_bread));
-            
+
             //check if user is login
             if (!Auth::instance()->logged_in( $request->controller(), $request->action(), $request->directory()))
             {
-                Alert::set(Alert::ERROR, __('You do not have permissions to access '.$request->controller().' '.$request->action()));
+				Alert::set(Alert::ERROR, sprintf(__('You do not have permissions to access %s'), $request->controller().' '.$request->action()));
                 $url = Route::url('oc-panel',array(   'controller' => 'auth',
                                                     'action'     => 'login'));
                 $this->redirect($url);
