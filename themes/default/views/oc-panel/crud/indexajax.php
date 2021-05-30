ba3dca8363ece8dd26d4e76b0864a8dbbae64dd9
Oliver Jimenez-Servin <oliverds@users.noreply.github.com>
2021-05-30 15:49:53 -0500

    Fix bootgrid actions dropdown menu

--- themes/default/views/oc-panel/crud/indexajax.php
+++ themes/default/views/oc-panel/crud/indexajax.php
@@ -124,7 +124,7 @@
     <div class="flex flex-col">
         <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
             <div class="align-middle inline-block min-w-full overflow-hidden">
-                <table id="grid-data-api" class="min-w-full" style="min-height: 300px;">
+                <table id="grid-data-api" class="min-w-full">
                     <thead>
                         <tr>
                             <?foreach($fields as $field):?>
